993aeff439d14e48dec072a7134ddf88ff3f871b
Shreya Garud <shreyagarud4@gmail.com>
2026-07-31 23:43:23 +0530

    first commit

---
+++ Variables & Data Types/example3.js
@@ -0,0 +1,12 @@
+// Swap two numbers without a third variable.
+
+let a = 10;
+let b = 20;
+
+a = a + b ; // 10+20=30
+
+b = a - b ; //30-20=10
+
+a = a - b ; //30-10=20
+
+console.log("a:",a, "b:",b) // a:20 b:10 
---
+++ Variables & Data Types/example6.js
@@ -0,0 +1,28 @@
+// convert a number to a string
+
+let num = 12346;
+console.log(typeof num)
+
+let str = String(num);
+console.log(str)
+
+console.log(typeof str)
+
+
+
+
+
+// Convert a number to a string
+
+let age = 25;
+
+// Before conversion
+console.log(age);
+console.log(typeof age);
+
+// Convert number to string
+let ageString = String(age);
+
+// After conversion
+console.log(ageString);
+console.log(typeof ageString);
