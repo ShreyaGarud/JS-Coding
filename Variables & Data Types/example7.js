da6e7d9a63591ba78563e533c48e353e85f250e9
Shreya Garud <shreyagarud4@gmail.com>
2026-08-01 22:57:24 +0530

    first commit

---
+++ Variables & Data Types/example7.js
@@ -0,0 +1,9 @@
+// Check if a variable is null or undefined.
+
+let value = 24;
+
+if (value === undefined) {
+    console.log("Value is undefined");
+} else {
+    console.log("Value is not undefined");
+}
