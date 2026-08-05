b2851d2ed9f10ecdc60f6d1900bf0263562862c9
Shreya Garud <shreyagarud4@gmail.com>
2026-08-05 18:22:31 +0530

    oc

---
+++ Operators_Conditionals/example1.js
@@ -0,0 +1,9 @@
+//Check if a number is even or odd.
+
+let num = 4;
+
+if (num % 2 ===0){
+    console.log("Even number");
+}else{
+    console.log("Odd number");
+}
---
+++ Operators_Conditionals/example2.js
@@ -0,0 +1,26 @@
+//check if a number is positive , negative or zero.
+
+let num = 122;
+
+if(num > 0 ){
+    console.log("number is positive");
+}else if(num < 0){
+    console.log("number is negative");
+}else{
+    console.log("number is zero")
+}
+
+
+/// if num > 0 than +ve AND num < 0 than -ve 
+
+////////////////////////////////////////////////
+
+let value = 24;
+
+if(value > 0){
+    console.log("positive");
+}else if(value < 0){
+    console.log("negative");
+}else{
+    console.log("zero");
+}
